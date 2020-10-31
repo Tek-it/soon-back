52a4d16832107b700addce62c250ad9861f6be6c
alitarfa <alitarfa123@gmail.com>
2020-10-31 14:14:35 +0100

    fix some problems

--- src/main/java/soon/io/soon/DTO/item/ItemDTO.java
+++ src/main/java/soon/io/soon/DTO/item/ItemDTO.java
@@ -1,5 +1,13 @@
 package soon.io.soon.DTO.item;
 
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ItemDTO {
     private int id;
     private Long dish;
--- src/main/java/soon/io/soon/models/item/Item.java
+++ src/main/java/soon/io/soon/models/item/Item.java
@@ -1,10 +1,15 @@
 package soon.io.soon.models.item;
 
+import lombok.*;
 import soon.io.soon.models.dish.Dish;
+
+import javax.persistence.*;
+
 
 public class Item {
 
     private int id;
+
     private Dish dish;
     private int quantity;
 }
